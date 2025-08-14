ad5e6256259d66a77ec5e4bf6eefb6c7bdc15b9b
almirsoares <almir.soares.grdtel@gmail.com>
2025-08-14 12:44:45 -0300

    alterando inicializador e requirements

--- start_streamlit.bat
+++ start_streamlit.bat
@@ -1,5 +1,11 @@
 @echo off
+echo Iniciando FastAPI (uvicorn)...
 cd /d C:\Users\central\Desktop\streamlit\DASH CRD
 call C:\Python39\Scripts\activate
+
+start uvicorn auth_api:app --reload --port 8002
+
+timeout /t 5 /nobreak > nul
+
 echo Iniciando Streamlit...
 streamlit run src/dashboard/app.py --server.port 9000
